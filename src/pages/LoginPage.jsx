be2dc63040319051b35e8f97bd370a5b9f8108eb
phoenix850508 <tom850508@gmail.com>
2023-05-24 16:46:48 +0800

    feat: login api function/add login funciton to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,20 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from 'api/auth';
 
 const LoginPage = () => {
-  const [userName, setUserName] = useState('');
+  const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const handleClick = async () => {
+    if (username.length === 0 || password.length === 0) {
+      return;
+    }
+    const { success, authToken } = await login({ username, password });
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+    }
+  };
   return (
     <AuthContainer>
       <div>
@@ -23,7 +33,7 @@ const LoginPage = () => {
         <AuthInput
           label={'帳號'}
           placeholder={'請輸入帳號'}
-          value={userName}
+          value={username}
           onChange={(nameInputValue) => setUserName(nameInputValue)}
         />
       </AuthInputContainer>
@@ -37,7 +47,7 @@ const LoginPage = () => {
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <Link to="/signup">
         <AuthLinkText>註冊</AuthLinkText>
       </Link>
